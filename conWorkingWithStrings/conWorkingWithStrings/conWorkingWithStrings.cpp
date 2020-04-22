e01e979d7f5f5a623399fb9d6ff377e09b82ca27
David De La Fuente <dell92054@gmail.com>
2020-04-22 06:30:03 -0500

    Completed exercises for Chapter 1 of Accelerated C++

--- conWorkingWithStrings/conWorkingWithStrings/conWorkingWithStrings.cpp
+++ conWorkingWithStrings/conWorkingWithStrings/conWorkingWithStrings.cpp
@@ -2,19 +2,105 @@
 //
 
 #include <iostream>
+#include <string>
+using namespace std;
+
+//function declaration
+void greeting();
+void FramedGreeting();
+void variableCout13();
+void variableCout14();
+void concatExample15();
+void multipleGreeting16();
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    multipleGreeting16();
+    concatExample15();
+    variableCout14();
+    variableCout13();
+    greeting();
+    FramedGreeting();
+    return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void multipleGreeting16()
+{
+    std::cout << "What is your name? ";
+    std::string name;
+    std::cin >> name;
+    std::cout << "Hello, " << name
+        << std::endl << "And what is yours? ";
+    std::cin >> name;
+    std::cout << "Hello, " << name
+        << "; nice to meet you too!" << std::endl;
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+}
+
+void concatExample15()
+{
+    { std::string s = "a string";
+    { std::string x = s + ", really";
+    std::cout << s << std::endl; 
+    //x was orignally not in scope
+    std::cout << x << std::endl; }
+    }
+
+}
+void variableCout14()
+{
+    { const std::string s = "a string";
+    std::cout << s << std::endl;
+    { const std::string s = "another string";
+    std::cout << s << std::endl; }}
+
+}
+void variableCout13()
+{
+    { const std::string s = "a string";
+    std::cout << s << std::endl; }
+
+    { const std::string s = "another string";
+    std::cout << s << std::endl; }
+
+}
+
+
+void greeting()
+{
+    // ask for the person's name
+    std::cout << "Please enter your first name: ";
+    // read the name
+    std::string name; // define name
+    std::cin >> name; // read into
+    // write a greeting
+    std::cout << "Hello, " << name << "!" << std::endl;
+  
+}
+
+void FramedGreeting()
+{
+    std::cout << "Please enter your first name: ";
+    std::string name;
+    std::cin >> name;
+    // build the message that we intend to write
+    const std::string greeting = "Hello, " + name + "!";
+
+    // build the second and fourth lines of the output
+    const std::string spaces(greeting.size(), ' ');
+    const std::string second = "* " + spaces + " *";
+    // build the first and fifth lines of the output
+    const std::string first(second.size(), '*');
+    // write it all
+    std::cout << std::endl;
+    std::cout << first << std::endl;
+    std::cout << second << std::endl;
+    std::cout << "* " << greeting << " *" << std::endl;
+    std::cout << second << std::endl;
+    std::cout << first << std::endl;
+
+    
+
+}
+
+
